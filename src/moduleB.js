30b77f8d3699e575efb90852d6cd9cb71c605dd8
Javier López de Ancos <javierlopezdeancos@gmail.com>
2017-05-16 21:09:52 +0200

    fix mocha test

--- src/moduleB.js
+++ src/moduleB.js
@@ -1,5 +1,3 @@
-/* eslint no-console:0 */
-
 export default function () {
     const PI = 3.1416
     return PI
--- test/moduleA.js
+++ test/moduleA.js
@@ -2,13 +2,16 @@ import {expect} from 'chai'
 
 import moduleA from '../src/moduleA.js'
 
-let moduleB = require( '../src/moduleB.js' )
-const mock_moduleB = () => 3.1415
 const PI = 3.1416
-moduleB = mock_moduleB
+
+beforeEach( function () {
+} )
 
 describe( '#ModuleA', () => {
     it( 'run sould console PI', () => {
         expect( moduleA() ).to.equal( PI )
     } )
+} )
+
+afterEach( function () {
 } )
